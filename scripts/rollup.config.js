4ee67a93e4cd321ba2ea076424142ac2ff1834dd
Stefan Battiston <stefan.battiston@gmail.com>
2020-10-08 11:59:46 -0300

    Add missing packages to /dist for test website to function.

--- scripts/rollup.config.js
+++ scripts/rollup.config.js
@@ -1,8 +1,31 @@
 const generate = require('videojs-generate-rollup-config');
+const copy = require('rollup-plugin-copy');
 
 // see https://github.com/videojs/videojs-generate-rollup-config
 // for options
-const options = {};
+const options = {
+  plugins(defaults) {
+    return {
+      browser: defaults.browser.concat([
+        copy({
+          targets: [
+            {
+              src: [
+                "node_modules/video.js/dist/video-js.css",
+                "node_modules/video.js/dist/video.min.js",
+                "node_modules/videojs-mux/dist/videojs-mux.js",
+                "weights/*",
+              ],
+              dest: "dist"
+            }
+          ]
+        }),
+      ]),
+      module: defaults.module,
+      test: defaults.test,
+    };
+  }
+};
 const config = generate(options);
 
 // Add additonal builds/customization here!
